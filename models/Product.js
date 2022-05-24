e48b7a33d30407d3bb4ae05582268bd56a36389d
randronaco <randronaco1027@gmail.com>
2022-05-23 23:28:38 -0400

    cloned starter code

--- models/Product.js
+++ models/Product.js
@@ -4,20 +4,20 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model { }
+class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-    {
-        // define columns
-    },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'product',
-    }
+  {
+    // define columns
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
 );
 
 module.exports = Product;
