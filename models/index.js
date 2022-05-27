04dd1c6d3a10fce46f71bbd1d9813f794b3dd9f4
randronaco <randronaco1027@gmail.com>
2022-05-27 00:56:20 -0400

    setup gets for all 3

--- models/index.js
+++ models/index.js
@@ -14,16 +14,14 @@ Category.hasMany(Product, {
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  as: 'product_tag', 
-  foreignKey: 'category_id'
+  through: ProductTag, 
+  foreignKey: 'product_id'
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'product_tag',
-  foreignKey: 'tag_name'
+  foreignKey: 'tag_id'
 })
 
 module.exports = {
