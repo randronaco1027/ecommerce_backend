a7e5c7bf9d60fc13cce0b71e24eebca996865a8d
randronaco <randronaco1027@gmail.com>
2022-05-27 01:09:30 -0400

    established post routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,7 +31,7 @@ router.get('/:id', (req, res) => {
       attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
     }
   })
-    .then(dbCategoryData => res.json(dbCategoryData))
+    .then(dbTagData => res.json(dbTagData))
     .catch(err => {
       console.log(err)
       res.status(500).json(err)
@@ -40,6 +40,14 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+    Tag.create({
+      tag_name: req.body.tag_name 
+    })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
 });
 
 router.put('/:id', (req, res) => {
